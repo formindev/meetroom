550d4e2fc8c0fb479eaf11abbe3a1a8f377638e5
formindev <formindeve@gmail.com>
2020-04-20 18:21:02 +0400

    Added event entity

--- src/main/java/com/formindev/meetroom/domain/Event.java
+++ src/main/java/com/formindev/meetroom/domain/Event.java
@@ -5,9 +5,11 @@ import org.springframework.lang.Nullable;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.time.ZonedDateTime;
 
 @Entity
-public class Booking {
+@Table(name = "events")
+public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -23,8 +25,56 @@ public class Booking {
     private String description;
 
     @NotNull
-    private String startDate;
+    private ZonedDateTime startDate;
 
     @NotNull
-    private String finishDate;
+    private ZonedDateTime finishDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ZonedDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(ZonedDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public ZonedDateTime getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(ZonedDateTime finishDate) {
+        this.finishDate = finishDate;
+    }
 }
