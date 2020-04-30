b92c4a7db29c6b2cb4759f2a9ca4cdc9108a8639
formindev <formindeve@gmail.com>
2020-04-30 08:48:10 +0400

    Added 'throw' to UserService

--- src/main/java/com/formindev/meetroom/service/UserService.java
+++ src/main/java/com/formindev/meetroom/service/UserService.java
@@ -19,7 +19,7 @@ public class UserService implements UserDetailsService {
         User user = userRepository.findByUserName(userName);
 
         if (user == null)
-            new UsernameNotFoundException("User not found!");
+            throw new UsernameNotFoundException("User not found!");
 
         return user;
     }
